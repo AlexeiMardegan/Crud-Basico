81771c4c0e36ccd0ab871a3b643a9cb54db90f83
Alexei Mardegan <76541124+AlexeiMardegan@users.noreply.github.com>
2022-04-24 18:04:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="bootstrap2/css/bootstrap.min.css" rel="stylesheet">
     <link href="estilo.css" rel="stylesheet">
+    <link rel="shortcut icon" href="register_login_signup_icon_219991.ico" type="image/x-icon">
     <title>Sistema Crud</title>
 </head>
 
@@ -27,11 +28,11 @@
                     
                     <form action="index.php" method="POST">
                         <label class="form-label">Nome</label><br />
-                        <input type="text" name="nome" class="form-control" placeholder="Digite o seu nome" required /><br />
+                        <input type="text"  maxlength="38" name="nome" class="form-control" placeholder="Digite o seu nome" required /><br />
                         <label class="form-label">RG</label><br />
-                        <input type="number" name="rg" class="form-control" placeholder="Digite o seu RG" required /><br />
+                        <input type="number" max="1000000000" min="10000000" name="rg" class="form-control" placeholder="Digite o seu RG" required /><br />
                         <label class="form-label">Idade</label><br />
-                        <input type="number" name="idade" class="form-control" placeholder="Digite a sua idade" required /><br />
+                        <input type="number" max="111" name="idade" class="form-control" placeholder="Digite a sua idade" required /><br />
                         
                         <button type="submit" class="btn btn-outline-secondary" name="btgravar" id="btgravar">Gravar <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-save2" viewBox="0 0 16 16">
   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v4.5h2a.5.5 0 0 1 .354.854l-2.5 2.5a.5.5 0 0 1-.708 0l-2.5-2.5A.5.5 0 0 1 5.5 6.5h2V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
